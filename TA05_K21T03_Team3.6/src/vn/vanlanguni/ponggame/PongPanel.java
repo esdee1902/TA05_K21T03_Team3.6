686e590f62102f604f9c1444506b1810f9353789
HoaiCao <hoaicao14374@gmail.com>
2016-11-04 17:07:02 +0700

    fix #13 Thiếu trường hợp ký tự ‘P’

--- TA05_K21T03_Team3.6/src/vn/vanlanguni/ponggame/PongPanel.java
+++ TA05_K21T03_Team3.6/src/vn/vanlanguni/ponggame/PongPanel.java
@@ -274,7 +274,7 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 
 	public void keyPressed(KeyEvent e) {
 		if (showTitleScreen) {
-			if (e.getKeyChar() == 'p') {
+			if (e.getKeyChar() == 'p'||e.getKeyChar() == 'P') {
 				showTitleScreen = false;
 				playing = true;
 			}
